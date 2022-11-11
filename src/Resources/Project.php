e38a2cddaec5f6823ddba8b5d0ad9f512d0fffea
Erik Bernskiold <erik@bernskioldmedia.com>
2022-11-11 21:52:55 +0100

    Add project user assignment endpoints (#5)
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>
    
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- src/Resources/Project.php
+++ src/Resources/Project.php
@@ -19,6 +19,31 @@ class Project extends BaseResource
     use FiltersActive;
     use FiltersClient;
 
+    public function users(int $projectId): object
+    {
+        return $this->client->get($this->getEndpoint().'/'.$projectId.'/user_assignments', $this->query);
+    }
+
+    public function user(int $projectId, int $userId): object
+    {
+        return $this->client->get($this->getEndpoint().'/'.$projectId.'/user_assignments/'.$userId, $this->query);
+    }
+
+    public function addUser(int $projectId, array $data): object
+    {
+        return $this->client->post($this->getEndpoint().'/'.$projectId.'/user_assignments', $data);
+    }
+
+    public function updateUser(int $projectId, int $userId, array $data): object
+    {
+        return $this->client->patch($this->getEndpoint().'/'.$projectId.'/user_assignments/'.$userId, $data);
+    }
+
+    public function removeUser(int $projectId, int $userId): bool
+    {
+        return $this->client->delete($this->getEndpoint().'/'.$projectId.'/user_assignments/'.$userId);
+    }
+
     protected function getEndpoint(): string
     {
         return 'projects';
